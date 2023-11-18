7b6ab6bce3e5cfc1f13ec43b4f0420673148f84c
Albaric0que <albamrus@gmail.com>
2023-11-18 19:04:17 +0100

    Add ids to buttons modifying the layout to implement the logic in main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const display = document.querySelector(".calculator__display");
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const pressedButton = button.textContent;
+
+    if (display.textContent === "0") {
+      display.textContent = pressedButton;
+    } else {
+      display.textContent += pressedButton;
+    }
+  });
+});
