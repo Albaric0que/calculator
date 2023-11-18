408a544b887646199012b1efdd54ab9512d75176
Albaric0que <albamrus@gmail.com>
2023-11-18 19:28:34 +0100

    Implement calculator logic

--- main.js
+++ main.js
@@ -7,6 +7,16 @@ buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const pressedButton = button.textContent;
 
+    if (button.id === "clear") {
+      display.textContent = "0";
+      return;
+    }
+
+    if (button.id === "equal") {
+      display.textContent = eval(display.textContent);
+      return;
+    }
+
     if (display.textContent === "0") {
       display.textContent = pressedButton;
     } else {
